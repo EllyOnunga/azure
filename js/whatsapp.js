53ff1ffed3cb23e7ab83cb0d3674aa0c25fed9c1
Elly Onunga <onungaelly@gmail.com>
2026-01-19 14:22:54 +0300

    feat: add floating whatsapp button to all pages

---
+++ js/whatsapp.js
@@ -0,0 +1,8 @@
+(function () {
+    const whatsappHTML = `
+        <a href="https://wa.me/254710297603" class="floating-whatsapp" target="_blank" aria-label="Chat on WhatsApp">
+            <i class="fa-brands fa-whatsapp"></i>
+        </a>
+    `;
+    document.body.insertAdjacentHTML('beforeend', whatsappHTML);
+})();
